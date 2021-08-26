758e93d57b314a6963cf20c9c5507a6d512014d9
Maciek <maciek29.opozda@gmail.com>
2021-08-26 16:00:11 +0200

    -implementing tracking position logic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 import '/sass/styles.scss'
+import {makeLogger} from 'ts-loader/dist/logger';
+const startTrackingButton:HTMLButtonElement = document.querySelector('.start-button')
 
+
+const cordinates:unknown[] = []
+
+startTrackingButton.addEventListener('click',()=>{
+    navigator.geolocation.watchPosition((data:GeolocationPosition)=>{
+        console.log(data)
+        cordinates.push([data.coords.latitude , data.coords.longitude])
+        // set local storage //
+        window.localStorage.setItem('coordinates',JSON.stringify(cordinates))
+    }, e=> console.log(e.message),
+        {
+            enableHighAccuracy:true
+        })
+})
 
 
