4bab1ee6fffbae57929a6dc31762f5b590d61671
Maciek <maciek29.opozda@gmail.com>
2021-08-26 16:20:11 +0200

    implementing start, save to  coordinates to local storage, and clear tracking

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import '/sass/styles.scss'
 import {makeLogger} from 'ts-loader/dist/logger';
 const startTrackingButton:HTMLButtonElement = document.querySelector('.start-button')
+const map:HTMLElement = document.querySelector('.map-container');
+const stopTrackingButton:HTMLButtonElement = document.querySelector('.stop-button')
 
 
-const cordinates:unknown[] = []
+const savedCordinates = JSON.parse(window.localStorage.getItem('coordinates'))
+
+console.log('saved coordinates',savedCordinates)
+
+const cordinates:unknown[] = [];
+let watchId;
 
 startTrackingButton.addEventListener('click',()=>{
-    navigator.geolocation.watchPosition((data:GeolocationPosition)=>{
+   watchId =  navigator.geolocation.watchPosition((data:GeolocationPosition)=>{
         console.log(data)
         cordinates.push([data.coords.latitude , data.coords.longitude])
         // set local storage //
@@ -15,6 +22,14 @@ startTrackingButton.addEventListener('click',()=>{
         {
             enableHighAccuracy:true
         })
+    return watchId
+})
+
+// stop tracking
+stopTrackingButton.addEventListener('click', ()=>{
+    navigator.geolocation.clearWatch(watchId)
+    console.log('watch stopped')
+    window.localStorage.clear()
 })
 
 
