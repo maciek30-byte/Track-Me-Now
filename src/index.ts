7f855b55069e0c64eb189276e1a0d83a1d969421
Maciek <maciek29.opozda@gmail.com>
2021-08-31 13:30:57 +0200

    -add not suported functionality and min and max resolution

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import '/sass/styles.scss'
 import TrackHandler from './TrackHandler';
-
+const bodyelement = document.querySelector('body')
 const test = new TrackHandler()
 test.init()
+
+if(window.matchMedia('(min-width:1024px)').matches){
+    const message = document.createElement('div');
+    message.classList.add('not-supported')
+    bodyelement.appendChild(message)
+    console.log(' screen is to big')
+}
