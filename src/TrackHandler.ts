3075e2411d9593f6beb1ebc16c9a9044dce8c2c3
Maciek <maciek29.opozda@gmail.com>
2021-08-31 12:14:36 +0200

    - main functionality working properly

--- src/TrackHandler.ts
+++ src/TrackHandler.ts
@@ -11,6 +11,8 @@ class TrackHandler {
 
   constructor() {
     this.screen = document.querySelector(".position-container");
+    this.screen.innerHTML=` click start to get Data`
+    console.log("screeen  in constructor  ", this.screen);
   }
 
   init() {
@@ -19,27 +21,44 @@ class TrackHandler {
 
     startBtn.addEventListener("click", () => this.startTracking());
     stopBtn.addEventListener("click", () => this.stopTracking());
+    console.log("screen in init", this.screen);
+
   }
 
   startTracking() {
     this.watcherId = navigator.geolocation.watchPosition(
-      this.onSucces,
+      this.onSuccess,
       this.onErr
     );
-  }
-  stopTracking() {}
 
-  private convertToDate(timestamp: number): Date {
-    return new Date(timestamp * 1000);
   }
-  private onSucces(data) {
+  stopTracking() {
+    navigator.geolocation.clearWatch(this.watcherId);
+    this.screen.innerHTML = `Click the start button to get Data`
+  }
+
+  // this binding
+  private onSuccess = (data: GeolocationPosition) => {
     //think about this destruct//
     const { timestamp, coords } = data;
     const { latitude, longitude } = coords;
+    console.log("timestamp", this);
     MapHandler.initMap([longitude, latitude]);
+    this.screen.innerHTML= `
+    Your Position is longitude ${longitude} latitude ${latitude};
+    time of measurment ${this.convertTimeStamp(timestamp)}
+    `
+
   }
 
-  onErr() {}
+  onErr(e: GeolocationPositionError) {
+    this.screen.innerHTML = JSON.stringify(e.message);
+  }
+
+  private convertTimeStamp(timestamp:number){
+    return new Date(timestamp).toLocaleDateString('pl-PL')
+
+  }
 }
 
 export default TrackHandler;
